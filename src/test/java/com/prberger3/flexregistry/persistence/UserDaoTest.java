eec0ff2dc0ef8ed0a6b864084aa27097b349ff96
Paul Berger <pberger@madisoncollege.edu>
2022-04-28 22:28:48 -0500

    Add UserDaoTest

---
+++ src/test/java/com/prberger3/flexregistry/persistence/UserDaoTest.java
@@ -0,0 +1,33 @@
+package com.prberger3.flexregistry.persistence;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserDaoTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void saveOrUpdate() {
+    }
+
+    @Test
+    void insert() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void getAll() {
+    }
+}
