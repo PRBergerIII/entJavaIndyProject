7e73ec9537dc0c309eeb8ecc51df61f3151aa695
Paul Berger <pberger@madisoncollege.edu>
2022-04-28 23:26:22 -0500

    Update UserDaoTest

--- src/test/java/com/prberger3/flexregistry/persistence/UserDaoTest.java
+++ src/test/java/com/prberger3/flexregistry/persistence/UserDaoTest.java
@@ -1,5 +1,6 @@
 package com.prberger3.flexregistry.persistence;
 
+import com.prberger3.flexregistry.entity.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -7,8 +8,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class UserDaoTest {
 
+    GenericDao<User> genericDao;
+
+    /**
+     * Sets up a new DAO and recreates the test database before each test.
+     */
     @BeforeEach
     void setUp() {
+
+        Database database = Database.getInstance();
+        database.runSQL("cleanDb.sql");
+
+        genericDao = new GenericDao<>(User.class);
+
     }
 
     @Test
