44d6e5c02eea9660a20703d6afa29cb829d0da1b
Paul Berger <pberger@madisoncollege.edu>
2022-04-28 22:10:18 -0500

    Pull in SessionFactoryProvider and GenericDao

---
+++ src/main/java/com/prberger3/flexregistry/persistence/GenericDao.java
@@ -0,0 +1,82 @@
+package com.prberger3.flexregistry.persistence;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+public class GenericDao<T> {
+
+    private Class<T> type;
+    private final Logger logger = LogManager.getLogger(this.getClass());
+
+    public GenericDao(Class<T> type) {
+        this.type = type;
+    }
+
+    private Session getSession() {
+        return SessionFactoryProvider.getSessionFactory().openSession();
+    }
+
+    public <T>T getById(int id) {
+
+        Session session = getSession();
+        T entity = (T)session.get(type, id);
+        session.close();
+        return entity;
+
+    }
+
+    public void saveOrUpdate(T entity) {
+
+        Session session = getSession();
+        Transaction transaction = session.beginTransaction();
+        session.saveOrUpdate(entity);
+        transaction.commit();
+        session.close();
+
+    }
+
+    public int insert(T entity) {
+
+        int newId = 0;
+        Session session = getSession();
+        Transaction transaction = session.beginTransaction();
+
+        newId = (int)session.save(entity);
+        transaction.commit();
+        session.close();
+
+        return newId;
+
+    }
+
+    public void delete(T entity) {
+
+        Session session = getSession();
+        Transaction transaction = session.beginTransaction();
+        session.delete(entity);
+        transaction.commit();
+        session.close();
+
+    }
+
+    public List<T> getAll() {
+
+        Session session = getSession();
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+
+        CriteriaQuery<T> query = builder.createQuery(type);
+        Root<T> root = query.from(type);
+        List<T> list = session.createQuery(query).getResultList();
+        session.close();
+        return list;
+
+    }
+
+}
