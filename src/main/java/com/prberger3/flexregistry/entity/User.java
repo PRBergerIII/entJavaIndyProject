67807430f43eb3541b33dd11d57cecaadb41d4f7
Paul Berger <pberger@madisoncollege.edu>
2022-05-03 10:52:34 -0400

    Add fixmes for when pk class is ready

--- src/main/java/com/prberger3/flexregistry/entity/User.java
+++ src/main/java/com/prberger3/flexregistry/entity/User.java
@@ -34,9 +34,9 @@ public class User {
     private String addressVisibility;
     private String about;
     private boolean admin;
-    @OneToMany(mappedBy = "user_followed_id", fetch = FetchType.EAGER)
+    @OneToMany(mappedBy = "user_followed_id", fetch = FetchType.EAGER) // FIXME: 5/3/2022 change to pk
     private Set<User> usersFollowed = new HashSet<>();
-
+    @OneToMany(mappedBy = "follower ", fetch = FetchType.EAGER) // FIXME: 5/3/2022 change to pk
     private Set<User> followers = new HashSet<>();
 
     public User() {
