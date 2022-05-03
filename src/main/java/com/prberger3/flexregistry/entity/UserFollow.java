e0850c89ed4e617d78e638e5d597ba9bb9a23254
Paul Berger <pberger@madisoncollege.edu>
2022-05-03 11:15:34 -0400

    Add UserFollow

---
+++ src/main/java/com/prberger3/flexregistry/entity/UserFollow.java
@@ -0,0 +1,12 @@
+package com.prberger3.flexregistry.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity(name = "UserFollow")
+@Table(name = "user_follow")
+public class UserFollow {
+
+
+
+}
