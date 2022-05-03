ce1dfb7254d67cf481e66ab6f747b1b7831092cc
Paul Berger <pberger@madisoncollege.edu>
2022-05-03 11:28:02 -0400

    Update UserFollow

--- src/main/java/com/prberger3/flexregistry/entity/UserFollow.java
+++ src/main/java/com/prberger3/flexregistry/entity/UserFollow.java
@@ -1,10 +1,14 @@
 package com.prberger3.flexregistry.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity(name = "UserFollow")
 @Table(name = "user_follow")
+@AssociationOverrides({
+        @AssociationOverride(name = "primaryKey.follower",
+                joinColumns = @JoinColumn(name = "follower_id")),
+        @AssociationOverride(name = "primaryKey.userFollowed",
+                joinColumns = @JoinColumn(name = "user_followed_id")) })
 public class UserFollow {
 
 
