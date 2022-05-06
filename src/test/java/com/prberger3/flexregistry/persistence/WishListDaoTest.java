c3554e88423505920b2587f0ad26aeadac8849d5
Paul Berger <pberger@madisoncollege.edu>
2022-05-05 21:24:36 -0500

    Add WishListDaoTest

---
+++ src/test/java/com/prberger3/flexregistry/persistence/WishListDaoTest.java
@@ -0,0 +1,81 @@
+package com.prberger3.flexregistry.persistence;
+
+import com.prberger3.flexregistry.entity.User;
+import com.prberger3.flexregistry.entity.UserConnection;
+import com.prberger3.flexregistry.entity.WishList;
+import com.prberger3.flexregistry.entity.WishListItem;
+import com.prberger3.flexregistry.util.Database;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+// TODO: 4/30/2022 javadocs
+// TODO: 5/5/2022 do some tests
+
+class WishListDaoTest {
+
+    GenericDao<User> userDao;
+    GenericDao<WishList> wishListDao;
+    GenericDao<WishListItem> wishListItemDao;
+
+    /**
+     * Sets up a new DAO and recreates the test database before each test.
+     */
+    @BeforeEach
+    void setUp() {
+
+        Database database = Database.getInstance();
+        database.runSQL("cleanWishListTests.sql");
+
+        userDao = new GenericDao<>(User.class);
+        wishListDao = new GenericDao<>(WishList.class);
+        wishListItemDao = new GenericDao<>(WishListItem.class);
+
+    }
+
+    @Test
+    void getByIdSuccess() {
+
+        
+
+    }
+
+    @Test
+    void saveOrUpdateSuccess() {
+
+        
+
+    }
+
+    @Test
+    void insertSuccess() {
+
+       
+    }
+
+    @Test
+    void deleteSuccess() {
+
+        
+
+    }
+
+    @Test
+    void getAllSuccess() {
+
+        
+
+    }
+
+    @Test
+    void addFollowerSuccess() {
+
+        
+
+    }
+
+}
---
+++ src/test/resources/cleanWishListTests.sql
@@ -0,0 +1,24 @@
+delete from user;
+delete from wish_list;
+delete from wish_list_item;
+ALTER TABLE user AUTO_INCREMENT = 1;
+ALTER TABLE wish_list AUTO_INCREMENT = 1;
+ALTER TABLE wish_list_item AUTO_INCREMENT = 1;
+insert into user (id, username, first_name, last_name, email, street, city, state, zip, address_visibility, about, admin)
+values  (1, 'pberger', 'Paul', 'Berger', 'pberger@madisoncollege.edu', '123 main st', 'place', 'WI', '12324', 'private', 'I am a size 6', true),
+        (2, 'user1234', 'A', 'Dude', 'notthatdude@aol.com', '444 all over st', 'Los Angeles', 'CA', '91342', 'public', null, false),
+        (3, 'user123', 'some', 'user', 'user123@email.com', null, null, null, null, 'followers', 'I have an address but it''s not on here', false);
+insert into wish_list (id, owner_id, title, visibility, purchased_items_visibility, list_type, event_date)
+values  (1, 1, 'My Birthday List', 'public', true, 'Birthday', '2022-05-31'),
+        (2, 1, 'Christmas', 'public', false, 'Holiday', '2022-12-25'),
+        (3, 2, 'New Baby Boyyyeeeeee', 'followers', false, 'Shower', '2023-03-12'),
+        (4, 3, 'Treat Yo'' Self', 'private', true, null, null);
+insert into flexregistry_test.wish_list_item (id, wish_list_id, name, specific_item, details, priority, price_range, purchased, purchased_message)
+values  (6, 1, 'Hoosit', true, 'go to this link: (pretend this is a link)', 1, '50-100', false, null),
+        (7, 1, 'Whatsit', true, 'you know the one', 5, '10-20', true, 'Here you go!'),
+        (8, 2, 'Thing', false, 'the thing', 3, null, false, null),
+        (9, 2, 'Shirt', false, 'I like blue, so any blue', 4, null, false, null),
+        (10, 3, 'diapers', true, 'Only want huggies, but any amount will do', 2, null, false, null),
+        (11, 3, 'car seat', true, 'Only the best for my little champ', 5, '100000', false, null),
+        (12, 3, 'teething rings', false, 'Any will do', 2, '5-10', true, 'chomp chomp!'),
+        (13, 4, 'PS5', true, 'Link to stock monitoring site', 5, '499', false, null);
