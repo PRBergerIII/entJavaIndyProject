6ddc4495114993a539c42ac8c89cb99fdeccbc04
Paul Berger <pberger@madisoncollege.edu>
2022-05-11 15:17:50 -0500

    Add EditProfileServlet

---
+++ src/main/java/com/prberger3/flexregistry/controller/EditProfileServlet.java
@@ -0,0 +1,31 @@
+package com.prberger3.flexregistry.controller;
+
+import com.prberger3.flexregistry.entity.User;
+import com.prberger3.flexregistry.persistence.GenericDao;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class EditProfileServlet  extends HttpServlet {
+
+    /**
+     *  Handles HTTP GET requests.
+     *
+     *@param  request  the HttpServletRequest object
+     *@param  response  the HttpServletResponse object
+     *@throws ServletException  if there is a Servlet failure
+     *@throws IOException  if there is an IO failure
+     */
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+
+
+    }
+
+}
