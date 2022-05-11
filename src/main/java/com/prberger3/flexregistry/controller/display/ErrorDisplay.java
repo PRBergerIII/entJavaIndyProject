1a818024d4a1757b0801cb0dada695ff04058281
Paul Berger <pberger@madisoncollege.edu>
2022-05-11 08:27:36 -0500

    Reorganize error handler

--- src/main/java/com/prberger3/flexregistry/controller/display/ErrorDisplay.java
+++ src/main/java/com/prberger3/flexregistry/controller/display/ErrorDisplay.java
@@ -1,17 +1,12 @@
-package com.prberger3.flexregistry.controller;
+package com.prberger3.flexregistry.controller.display;
 
-import com.prberger3.flexregistry.entity.User;
-import com.prberger3.flexregistry.persistence.GenericDao;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-public class ErrorServlet extends HttpServlet {
+public class ErrorDisplay extends HttpServlet {
 // TODO: javadoc
     /**
      *  Handles HTTP GET requests.
