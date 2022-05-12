50af4d04de48db6520a3ebed130753e287211737
Paul Berger <pberger@madisoncollege.edu>
2022-05-11 23:39:33 -0500

    Add NewListServlet

--- src/main/java/com/prberger3/flexregistry/controller/EditProfileServlet.java
+++ src/main/java/com/prberger3/flexregistry/controller/EditProfileServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-public class EditProfileServlet  extends HttpServlet {
+public class EditProfileServlet extends HttpServlet {
 
     /**
      *  Handles HTTP GET requests.
---
+++ src/main/java/com/prberger3/flexregistry/controller/NewListServlet.java
@@ -0,0 +1,44 @@
+package com.prberger3.flexregistry.controller;
+
+import com.prberger3.flexregistry.entity.User;
+import com.prberger3.flexregistry.persistence.GenericDao;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class NewListServlet extends HttpServlet {
+
+    /**
+     * Handles HTTP GET requests.
+     *
+     * @param request  the HttpServletRequest object
+     * @param response the HttpServletResponse object
+     * @throws ServletException if there is a Servlet failure
+     * @throws IOException      if there is an IO failure
+     */
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+    /**
+     * Handles HTTP POST requests.
+     *
+     * @param request  the HttpServletRequest object
+     * @param response the HttpServletResponse object
+     * @throws ServletException if there is a Servlet failure
+     * @throws IOException      if there is an IO failure
+     */
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        
+
+    }
+
+}
